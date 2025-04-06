59709c96a10b840df224e2c37d81a719a9f2f1f2
Fausto Torres <fausto.alemao@gmail.com>
2025-04-06 19:55:20 -0300

    Update fetch-rn.js

--- fetch-rn.js
+++ fetch-rn.js
@@ -2,10 +2,14 @@ import { useEffect } from 'react';
 
 const App = () => {
   const initAsync = async () => {
-    //seems to work with others but failing with google maps
-    const response = await fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?sessiontoken=${sessionToken}&input=${text.replace(/\s+/g,"+")}&key=${this.API_KEY}&limit=10${t}&language=en`)
-    const json = await response.json()
-    console.log(json)
+    try{
+      //seems to work with others but failing with google maps
+      const response = await fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?sessiontoken=${sessionToken}&input=${text.replace(/\s+/g,"+")}&key=${this.API_KEY}&limit=10${t}&language=en`)
+      const json = await response.json()
+      console.log(json)
+    }catch(e){
+      console.log(e
+    }
   }
   useEffect(()=>{
     initAsync()
